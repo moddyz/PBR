0f1bb2d67cce782eb140b5e9ffce35ab14d8c5fb
moddyz <moddy3d@gmail.com>
2020-03-03 23:41:38 +1300

    'pbr_program' CMake macro for building executables.

--- cmake/macros/Public.cmake
+++ cmake/macros/Public.cmake
@@ -54,3 +54,50 @@ function(pbr_library PACKAGE_NAME)
     )
 
 endfunction() # pbr_library
+
+function(pbr_program PROGRAM_NAME)
+
+    set(options
+    )
+
+    set(oneValueArgs
+    )
+
+    set(multiValueArgs
+        CPPFILES
+        INCLUDE_PATHS
+        LIBRARIES
+    )
+
+    cmake_parse_arguments(args
+        "${options}"
+        "${oneValueArgs}"
+        "${multiValueArgs}"
+        ${ARGN}
+    )
+
+    target_include_directories(${PROGRAM_NAME}
+        PRIVATE ${CMAKE_BINARY_DIR}/include ${args_INCLUDE_PATHS}
+    )
+
+    target_link_libraries(
+        ${PROGRAM_NAME} PRIVATE ${args_LIBRARIES}
+    )
+
+    if (LINUX)
+        set_target_properties(${PROGRAM_NAME} PROPERTIES LINK_FLAGS "-Wl,-rpath,$ORIGIN/../lib/")
+    endif()
+
+    if (APPLE)
+        set_target_properties(${PROGRAM_NAME} PROPERTIES LINK_FLAGS "-Wl,-rpath,@executable_path/../lib/")
+    endif()
+
+    # Install
+    install(
+        TARGETS ${PROGRAM_NAME}
+        DESTINATION bin
+    )
+
+endfunction() # pbr_program
+
+
