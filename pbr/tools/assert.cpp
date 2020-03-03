833f5b0bb0cdd420f173e80feb2ed7a59c45bcbe
moddyz <moddy3d@gmail.com>
2020-03-04 00:17:24 +1300

    Documentation for PBR_ASSERT.  Demangle symbols.

--- pbr/tools/assert.cpp
+++ pbr/tools/assert.cpp
@@ -1,19 +1,45 @@
-#include <cstdlib>
+#include <cxxabi.h>
+#include <dlfcn.h>
 #include <execinfo.h>
+#include <signal.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 namespace pbr
 {
-void AssertHandler()
+void AssertHandler( const char* i_expression )
 {
-    void*  callstack[ 128 ];
-    int    numFrames, frames = backtrace( callstack, 128 );
-    char** strs = backtrace_symbols( callstack, frames );
-    for ( numFrames = 0; numFrames < frames; ++numFrames )
+    printf( "<PBR_ASSERT failed for expression: %s>\n", i_expression );
+
+    constexpr size_t c_maxStackDepth = 16;
+    void*            backtracePtrs[ c_maxStackDepth ];
+    int              stackDepth = backtrace( backtracePtrs, c_maxStackDepth );
+    for ( int stackIndex = 0; stackIndex < stackDepth; ++stackIndex )
     {
-        printf( "%s\n", strs[ numFrames ] );
+        Dl_info dynamicLinkInfo;
+        if ( !dladdr( backtracePtrs[ stackIndex ], &dynamicLinkInfo ) )
+        {
+            continue;
+        }
+
+        const char* symbolName;
+        symbolName = dynamicLinkInfo.dli_sname;
+
+        int   status;
+        char* demangledSymbol;
+        demangledSymbol = abi::__cxa_demangle( symbolName, NULL, 0, &status );
+        if ( status == 0 && demangledSymbol )
+        {
+            symbolName = demangledSymbol;
+        }
+
+        printf( "\t%s, %s\n", dynamicLinkInfo.dli_fname, symbolName );
+
+        if ( demangledSymbol )
+        {
+            free( demangledSymbol );
+        }
     }
-    free( strs );
 }
 
 } // namespace pbr
--- pbr/tools/assert.h
+++ pbr/tools/assert.h
@@ -1,11 +1,17 @@
 #pragma once
 
+/// \brief Assertion tools for printing function stack, allowing the developer
+/// to locate the source of error.
+///
+/// In Debug builds, PBR_ASSERT macro will print the stacktrace when the input expression evaluates to false.
+/// In Release builds, the PBR_ASSERT macro will be pre-processed away in Release builds.
+
 /* clang-format off */
 #ifdef PBR_DEBUG
-#define PBR_ASSERT(expr)        \
-    if ( !expr )                \
-    {                           \
-        pbr::AssertHandler();   \
+#define PBR_ASSERT(expr)           \
+    if ( !expr )                   \
+    {                              \
+        pbr::AssertHandler(#expr); \
     }
 #else
 #define PBR_ASSERT(expr) void()
@@ -14,7 +20,7 @@
 
 namespace pbr
 {
-void AssertHandler();
+void AssertHandler( const char* i_expression );
 
 } // namespace pbr
 
