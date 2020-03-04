693e1828f5b41d6080618c725363cba650919c69
moddyz <moddy3d@gmail.com>
2020-03-04 20:39:25 +1300

    Included backward.hpp for printing pretty stacktraces.

---
+++ pbr/tools/README.md
@@ -0,0 +1,7 @@
+# Tools
+
+A collection of tools to aid with the development process.
+
+## Third party libraries used
+
+- https://github.com/bombela/backward-cpp
--- pbr/tools/assert.cpp
+++ pbr/tools/assert.cpp
@@ -1,45 +1,19 @@
-#include <cxxabi.h>
-#include <dlfcn.h>
-#include <execinfo.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
+#define BACKWARD_HAS_DW 1
+
+#include "backward.hpp"
 
 namespace pbr
 {
 void AssertHandler( const char* i_expression )
 {
     printf( "<PBR_ASSERT failed for expression: %s>\n", i_expression );
-
-    constexpr size_t c_maxStackDepth = 16;
-    void*            backtracePtrs[ c_maxStackDepth ];
-    int              stackDepth = backtrace( backtracePtrs, c_maxStackDepth );
-    for ( int stackIndex = 0; stackIndex < stackDepth; ++stackIndex )
-    {
-        Dl_info dynamicLinkInfo;
-        if ( !dladdr( backtracePtrs[ stackIndex ], &dynamicLinkInfo ) )
-        {
-            continue;
-        }
-
-        const char* symbolName;
-        symbolName = dynamicLinkInfo.dli_sname;
-
-        int   status;
-        char* demangledSymbol;
-        demangledSymbol = abi::__cxa_demangle( symbolName, NULL, 0, &status );
-        if ( status == 0 && demangledSymbol )
-        {
-            symbolName = demangledSymbol;
-        }
-
-        printf( "\t%s, %s\n", dynamicLinkInfo.dli_fname, symbolName );
-
-        if ( demangledSymbol )
-        {
-            free( demangledSymbol );
-        }
-    }
+    backward::StackTrace stackTrace;
+    stackTrace.load_here( 32 );
+    backward::Printer printer;
+    printer.object     = true;
+    printer.color_mode = backward::ColorMode::always;
+    printer.address    = true;
+    printer.print( stackTrace, stderr );
 }
 
 } // namespace pbr
