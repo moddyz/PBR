99e2ed67822046bbba47b349fa9b435350589ad0
moddyz <moddy3d@gmail.com>
2020-03-06 23:46:09 +1300

    Printf logging facility.. until we can get optional variadic arguments to work.

--- pbr/tools/log.h
+++ pbr/tools/log.h
@@ -4,21 +4,6 @@
 
 /// \briief Logging utilities
 
-#define PBR_LOG_INFO( format, ... ) pbr::Log( pbr::LogLevel::Info, format, ## __VA_ARGS__ );
-#define PBR_LOG_WARN( format, ... ) pbr::Log( pbr::LogLevel::Warn, format, ## __VA_ARGS__ );
-#define PBR_LOG_ERROR( format, ... ) pbr::Log( pbr::LogLevel::Error, format, ## __VA_ARGS__ );
-
-namespace pbr
-{
-/// Levels of logging.
-enum class LogLevel : int
-{
-    Info = 0,
-    Warn,
-    Error,
-};
-
-/// Logging utility with variadic arguments.
-void Log( LogLevel i_logLevel, const char* i_format, ... );
-
-} // namespace pbr
+#define PBR_LOG_INFO( format, ... ) printf( format, ## __VA_ARGS__ );
+#define PBR_LOG_WARN( format, ... ) printf( format, ## __VA_ARGS__ );
+#define PBR_LOG_ERROR( format, ... ) printf( format, ## __VA_ARGS__ );
